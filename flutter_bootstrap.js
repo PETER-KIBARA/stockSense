9c279fee7641bafb10699709c30f50209abaf510
PETER-KIBARA <PETER-KIBARA@users.noreply.github.com>
2026-08-30 13:56:13 +0000

    deploy: 6fab0003ec32ef0d8a6c0ad3f2d4fd3a7643da39

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "727279336"
+    serviceWorkerVersion: "439559078"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,8 +31,8 @@ const RESOURCES = {"version.json": "e2d8fda4d1b1112b3779e6f2d2768dd2",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
-"main.dart.js": "57d0a3178bbed4896f10996a4e365273",
-"flutter_bootstrap.js": "d6ff75cb7e118ed83ab113d98dfe8ef5",
+"main.dart.js": "28d0d3406392595073304e6d5354c86d",
+"flutter_bootstrap.js": "a7fa20371200180de5660308b20cf01c",
 "manifest.json": "578f40c2a5bc912e957ee75c90b0f6a3"};
 // The application shell files that are downloaded before a service worker can
 // start.
